515b6433728c21a0c9274bf6c8d5c1a33ea98249
kaz <fahirah.diarra@gmail.com>
2020-03-28 08:34:05 -0400

    corrected returned text format

--- src/OutilCorrection.java
+++ src/OutilCorrection.java
@@ -95,7 +95,7 @@ public class OutilCorrection {
                     }
 
                     else {
-                        suggestiontext += suggestions.get(j);
+                        suggestiontext += suggestions.get(j)+",";
                     }
                 }
 
